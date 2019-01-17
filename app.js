655d80bca13d3e56722a75f24551fa63a747d4a3
Miroslav Zdravkovic <miroslav@dudesquare.nl>
2019-01-17 12:09:12 +0100

    Final commit

--- app.js
+++ app.js
@@ -2,35 +2,79 @@ new Vue({
     el: '#game',
     data() {
         return {
-            message: 'Simple game',
-            width: 0,
-            barWidth: 0,
-            stop: 100
+            message: 'Hero game',
+            showButton: true,
+            myHealth: 0,
+            monsterHealth: 0,
+            damageDone: [],
+            visible: true
         }
     },
     methods: {
-        progressBar() {
-            setInterval(() => {
-                if (this.width <= this.stop) {
-                    this.barWidth = this.width++ + '%'
-                    console.log(this.barWidth)
-                }
-            }, 50);
+        startGame() {
+            this.myHealth = 100
+            this.monsterHealth = 100
+            this.showButton = false
         },
-        myClass() {
+        myAttackStyle() {
             return {
-                width: this.barWidth,
+                width: this.myHealth + '%',
+                height: '100%',
+                backgroundColor: 'olive'
+            }
+        },
+        monsterAttackStyle() {
+            return {
+                width: this.monsterHealth + '%',
                 height: '100%',
                 backgroundColor: 'olive'
             }
         },
         attack() {
-            this.width -= 10
+            this.damage(10, 15)
         },
-        reset() {
-            this.barWidth = 0
+        specialAttack() {
+            this.damage(10, 40)
+        },
+        damage(val1, val2) {
+            this.myHealth -= Math.floor(Math.random() * val1);
+            this.monsterHealth -= Math.floor(Math.random() * val2);
+            this.damageDone.push(100 - this.myHealth)
+            this.damageDone.push(100 - this.monsterHealth)
+            console.log(JSON.stringify(this.damageDone))
+            console.log(JSON.stringify(this.damageDone))
+        },
+        giveUp() {
+            this.showButton = true
+            this.damageDone = []
+        },
+        heal() {
+            if (this.myHealth < 95) {
+                this.myHealth += 5;
+                setTimeout(() => {
+                    this.myHealth -= 4;
+                }, 1000);
+                console.log(this.myHealth)
+            }
+        },
+        myAlert(value, text) {
+            if (value <= 0) {
+                if (confirm(text)) {
+                    this.giveUp()
+                } else {
+                    console.log('nothing')
+                }
+            }
         }
     },
     computed: {
+    },
+    watch: {
+        myHealth(value) {
+            this.myAlert(value, 'MONSTER IS THE WINNER!')
+        },
+        monsterHealth(value) {
+            this.myAlert(value, 'VICTORY!')
+        }
     }
 })
